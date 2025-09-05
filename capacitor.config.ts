4ce16f73772068aa53e5415c17037dbc51cab39e
Leonel Gregio Auler <leonel.lga@gmail.com>
2025-09-05 13:34:13 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Conexão 1º Igreja Batista Taquara',
+  webDir: 'www'
+};
+
+export default config;
